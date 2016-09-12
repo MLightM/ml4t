c35bf601ba0acf68a50d94281ef4d5089e19dcfa
MST <MST@MSTs-MacBook-Pro.local>
2016-09-12 22:15:28 +0800

    marketsim.py version 1 works, save

--- mc2_p1/marketsim.py
+++ mc2_p1/marketsim.py
@@ -5,18 +5,50 @@ import numpy as np
 import datetime as dt
 import math
 import os
+import copy
 from util import get_data, plot_data
 
-def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000):
+def compute_portvals(orders_file = "./orders/orders-leverage-3.csv", start_val = 1000000):
     # this is the function the autograder will call to test your code
     # TODO: Your code here
+    longs = 0
+    shorts = 0
+    cash = start_val
+    Symbols = {}
     ordersfromfile = pd.read_csv(orders_file, index_col='Date', parse_dates=True, na_values=['nan'])
+    print ordersfromfile
     for date, row in ordersfromfile.iterrows():
-        symbol = list()
-        symbol.append(row['Symbol'])
+    	old_Symbols = copy.deepcopy(Symbols)
+        old_cash = cash
+        symbol = row['Symbol']
+        if symbol not in Symbols:
+        	Symbols[symbol] = 0
         dates = pd.date_range(date, date)
-    	df = get_data(symbol, dates, addSPY=False)     # get that row's date, symbol, price
-    	security_price = df.values[0][0]
+    	df = get_data(Symbols.keys(), dates, addSPY=False)     # get that row's date, symbol, price
+    	# print df
+        security_price = df[symbol].values[0]
+        trans_price = security_price*row['Shares']
+    	if row['Order'] == 'BUY':
+    		Symbols[symbol] += row['Shares']
+    		cash -= trans_price
+        else:
+    		Symbols[symbol] -= row['Shares']
+    		cash += trans_price
+    	
+        portfolio_stats = df.values[0] * np.array(Symbols.values())
+        longs = portfolio_stats[portfolio_stats>=0]
+        shorts = portfolio_stats[portfolio_stats<0]
+
+        # print Symbols, "--------Portfoilo-------",'\n'
+        # print date, row, "--------Current Trading-------",'\n'
+        print "Portvals: {}, Cash: {}, Longs: {}, Shorts: {}".format(cash+longs.sum()+shorts.sum(), cash, longs.sum(), shorts.sum()), '\n'
+        leverage = compute_leverage(longs, shorts, cash)
+        if leverage > 2:
+        	Symbols = old_Symbols
+        	# longs = old_longs
+        	# shorts = old_shorts
+        	cash = old_cash
+
     	
 
     # In the template, instead of computing the value of the portfolio, we just
@@ -29,9 +61,9 @@ def compute_portvals(orders_file = "./orders/orders.csv", start_val = 1000000):
     #return portvals
 
 def compute_leverage(longs, shorts, cash):
-	return (math.sum(longs) + sum(math.abs(shorts))) / (math.sum(longs) - math.sum(math.abs(shorts)) + cash)
+	return (sum(longs) - sum(abs(shorts))) / (sum(longs) + sum(abs(shorts)) + cash)
 
-def 
+# def 
 
 def test_code():
     # this is a helper function you can use to test your code
